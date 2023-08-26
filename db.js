fafa7aa331199f9d1159417091216d89f6548b0c
FishySenpai <akanomanbasit@gmail.com>
2023-08-26 10:25:56 +0500

    connected nodejs with aiven postgres db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 const pool = new Pool({
-    user: "postgres",
-    password: process.env.PASSWORD,
-    host: "localhost",
-    post: 5432,
-    database: "voiceai"
+  user: "avnadmin",
+  password: "",
+  host: "voice-ai-voice-ai.aivencloud.com",
+  post: 27157,
+  database: "voiceai",
 });
 module.exports = pool;
---
+++ text.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const { Pool } = require("pg");
+const url = require("url");
+require("dotenv").config();
+
+const postgresqlUri = process.env.postgresqlUri;
+
+const conn = new URL(postgresqlUri);
+conn.search = "";
+
+const pool = new Pool({
+  connectionString: conn.href,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync("./ca.pem").toString(),
+  },
+});
+
+// Export the pool so you can use it in other parts of your application
+module.exports = pool;
+
+// You don't need the client connection code here anymore
+// The connection pool will manage connections for you
