3d2c57af685e8816520348a63cba8e1736db1b30
FishySenpai <akanomanbasit@gmail.com>
2023-08-28 20:34:05 +0500

    added api fetch func for text

--- index.js
+++ index.js
@@ -2,22 +2,62 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const pool = require('./text')
+const axios = require("axios");
+require("dotenv").config();
 const fs = require("fs");
 app.use(cors());
 app.use(express.json());
 
-app.post("/text", async (req, res)=>{
-    {
-        try{
-            const {description} = req.body;
-             const audioData = fs.readFileSync("output.mp3");
-            const newText = await pool.query("INSERT INTO text (description, audio) VALUES($1, $2) RETURNING *", [description, audioData]);
-            res.json(newText)
-        } catch(err) {
-            console.error(err.message)
-        }
+app.post("/text", async (req, res) => {
+  try {
+    const { description } = req.body;
+
+    // Make a request to the external API to generate audio
+    const apiUrl =
+      "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM"; // Replace with your actual API endpoint
+    const xiApiKey = process.env.xiApiKey; // Replace with your actual API key
+
+    const requestData = {
+      text: description, // Use the text from the request body
+      model_id: "eleven_monolingual_v1",
+      voice_settings: {
+        stability: 0.5,
+        similarity_boost: 0.5,
+      },
+    };
+
+    const headers = {
+      accept: "audio/mpeg",
+      "xi-api-key": xiApiKey,
+      "Content-Type": "application/json",
+    };
+
+    const response = await axios.post(apiUrl, requestData, {
+      headers,
+      responseType: "arraybuffer",
+    });
+
+    // Handle the API response here
+    if (response.status === 200) {
+      const audioData = response.data;
+
+      // Insert both the text and audio data into the database
+      const newText = await pool.query(
+        "INSERT INTO text (description, audio) VALUES ($1, $2) RETURNING *",
+        [description, audioData]
+      );
+
+      res.json(newText);
+    } else {
+      console.error("API request failed with status:", response.status);
+      res.status(500).json({ error: "Failed to generate audio" });
     }
-})
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 app.get("/all", async (req, res) => {
   {
     try {
